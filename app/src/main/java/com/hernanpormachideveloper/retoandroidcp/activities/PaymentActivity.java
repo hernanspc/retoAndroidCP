111c67d9dbb5d9b253f68469009ec894ebe5fbed
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2024-05-06 10:11:59 -0500

    pasando datos a screen payment

--- app/src/main/java/com/hernanpormachideveloper/retoandroidcp/activities/PaymentActivity.java
+++ app/src/main/java/com/hernanpormachideveloper/retoandroidcp/activities/PaymentActivity.java
@@ -3,6 +3,7 @@ package com.hernanpormachideveloper.retoandroidcp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.hernanpormachideveloper.retoandroidcp.R;
 
@@ -12,5 +13,13 @@ public class PaymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        // Recuperar el precio total del intent
+        double totalPrice = getIntent().getDoubleExtra("totalPrice", 0.0);
+
+        // Hacer lo que necesites con el precio total, como mostrarlo en un TextView
+        // Por ejemplo:
+        TextView totalPriceTextView = findViewById(R.id.txtTotalPrice);
+        totalPriceTextView.setText(String.valueOf(totalPrice));
     }
 }
